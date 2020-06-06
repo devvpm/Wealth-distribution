214506f0eac1d7a99f33acd6c55ffafc3ba7b4e2
Vinod <vinoditb@gmail.com>
2020-06-06 10:52:27 +0000

    Better Coin Flip logic. And some bonus to ppl, we have doubled the initial handout. :)

--- wealth_distribution.py
+++ wealth_distribution.py
@@ -1,23 +1,32 @@
 import random
 import json
 import time
+import numpy as np
 
-# We distribute wealth among 10 people equally. Give 10$ each.
+def coinFlip():    
+    # Probablity 
+    p = .5
+    #perform the binomial distribution (returns 0 or 1)    
+    result = np.random.binomial(1,p)
+    return result
+
+
+# We distribute wealth among 10 people equally. Give 20$ each.
 people_wealth = {
-    "person_1": 10,
-    "person_2": 10,
-    "person_3": 10,
-    "person_4": 10,
-    "person_5": 10,
-    "person_6": 10,
-    "person_7": 10,
-    "person_8": 10,
-    "person_9": 10,
-    "person_10": 10
+    "person_1": 20,
+    "person_2": 20,
+    "person_3": 20,
+    "person_4": 20,
+    "person_5": 20,
+    "person_6": 20,
+    "person_7": 20,
+    "person_8": 20,
+    "person_9": 20,
+    "person_10": 20
 }
 
 round = 1
-while round <= 200:
+while round <= 300:
 
     # Betting starts in a round robin fashion.
     for person, persons_wealth in people_wealth.items():
@@ -40,19 +49,22 @@ while round <= 200:
                     break
 
                 # We pick a random number. 
-                random_number = random.randint(0,99)
-            
+                head_or_tail = coinFlip()
+                # 0 - Head
+                # 1 Tail
                 # Person 1 wins if its Odd, Person 2 wins if its Even.
-                if (random_number % 2) == 0:
+                if head_or_tail == 0:
+                    result = "Head"
                     wealth_of_opponent = wealth_of_opponent + 1
                     persons_wealth = persons_wealth - 1
                     winner = person_betting_against
                 else:
+                    result = "Tail"
                     wealth_of_opponent = wealth_of_opponent - 1
                     persons_wealth = persons_wealth + 1
                     winner = person
                 
-                print ("Betting between %s and %s: Winner is %s" % (person, person_betting_against, winner))
+                print ("Betting between %s and %s: It's %s! and the winner is %s" % (person, person_betting_against, result, winner))
 
                 people_wealth[person_betting_against] = wealth_of_opponent
                 people_wealth[person] = persons_wealth
